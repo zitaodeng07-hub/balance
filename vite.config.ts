427b9507cabafe5a1b4666483f21801700b5580d
EXP <2597696824@qq.com>
2026-05-13 00:47:14 +0800

    Improve iPhone Safari compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: ['es2018', 'safari13'],
+    cssTarget: 'safari13',
+  },
 });
